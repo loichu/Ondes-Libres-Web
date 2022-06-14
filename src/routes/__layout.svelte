1fec30f5e5857b2470c3e5ef425ffe2bc2a860e8
loichu <loic@humbert-web.dev>
2022-06-14 10:05:18 +0200

    Init pnpm, sveltekit, typescript

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,6 @@
+<nav>
+    <a href="/">Home</a>
+    <a href="/contact">Contactez-nous</a>
+</nav>
+
+<slot></slot>
---
+++ src/routes/contact.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+  <title>Ondes Libres - Contact</title>
+</svelte:head>
+
+<h1>Qui que vous soyez, n'hésitez pas à nous poser vos questions</h1>
